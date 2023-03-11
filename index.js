acfda0a778db4543725d415583245821fec0fda2
Ryomi-j <ryomi.j78@gmail.com>
2023-03-11 09:56:00 +0900

    re-design file construction : make a new file for eventListeners

--- index.js
+++ index.js
@@ -1,35 +1,9 @@
-import { selectDate } from './modules/selectDate.js';
+import { events } from './modules/eventListener.js';
 import { calendar } from './render/calendar.js';
-import { $body, $monthDays, calendarFrame, date, nextMonth, prevMonth, showDate } from './vars/dom.js';
 
 function init() {
   calendar();
-
-  $body.addEventListener('click', (e) => {
-    calendarFrame.style.removeProperty('--display', 'block');
-  });
-
-  showDate.addEventListener('click', (e) => {
-    e.stopPropagation();
-    calendarFrame.style.setProperty('--display', 'block');
-  });
-
-  $monthDays.addEventListener('click', (e) => {
-    e.stopPropagation();
-    selectDate(e);
-  });
-
-  prevMonth.addEventListener('click', (e) => {
-    e.stopPropagation();
-    date.setMonth(date.getMonth() - 1);
-    calendar();
-  });
-
-  nextMonth.addEventListener('click', (e) => {
-    e.stopPropagation();
-    date.setMonth(date.getMonth() + 1);
-    calendar();
-  });
+  events()
 }
 
 init();
---
+++ modules/eventListener.js
@@ -0,0 +1,31 @@
+import { calendar } from '../render/calendar.js';
+import { $body, $monthDays, calendarFrame, date, nextMonth, prevMonth, showDate } from '../vars/dom.js';
+import { selectDate } from './selectDate.js';
+
+export function events() {
+  $body.addEventListener('click', (e) => {
+    calendarFrame.style.removeProperty('--display', 'block');
+  });
+
+  showDate.addEventListener('click', (e) => {
+    e.stopPropagation();
+    calendarFrame.style.setProperty('--display', 'block');
+  });
+
+  $monthDays.addEventListener('click', (e) => {
+    e.stopPropagation();
+    selectDate(e);
+  });
+
+  prevMonth.addEventListener('click', (e) => {
+    e.stopPropagation();
+    date.setMonth(date.getMonth() - 1);
+    calendar();
+  });
+
+  nextMonth.addEventListener('click', (e) => {
+    e.stopPropagation();
+    date.setMonth(date.getMonth() + 1);
+    calendar();
+  });
+}
