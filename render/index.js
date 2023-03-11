be5437614490498f3f83f2ff2c88f06584771cf2
Ryomi-j <ryomi.j78@gmail.com>
2023-03-11 09:31:37 +0900

    make a folder for managing variables

--- render/index.js
+++
@@ -1,5 +0,0 @@
-import { getDOM } from "../utils/editDOM.js";
-
-export const date = new Date();
-
-export let $monthDays = getDOM('.days');
---
+++ vars/dom.js
@@ -0,0 +1,9 @@
+import { getDOM } from "../utils/editDOM.js";
+
+export const date = new Date();
+
+export let $monthDays = getDOM('.days');
+
+export const $body = getDOM('body');
+export const showDate = getDOM('.showDate');
+export const calendarFrame = getDOM('.calendar');
